48b0b0eaaf4d8e396dc0b595d8d7a20c0ae2cd2a
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-03-21 14:43:40 +0100

    Create api.js

---
+++ __tests__/api.js
@@ -0,0 +1,10 @@
+// Run api.js in your node console
+// Read https://github.com/facebook/mention-bot/issues/94!
+
+mentionBot.guessOwnersForPullRequest("https://github.com/bunto/mention-bot", 3, "SuriyaaKudoIsc", "master", {}, github)
+.then(function(users) {   
+    console.log(users);
+})
+.catch(function(err) {
+console.error(err);
+});
